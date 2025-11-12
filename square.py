b1625ccdca14322a94ae793bb589e1ac3cdf3e49
B1GSano <leokur1234@mail.ru>
2025-11-12 20:54:02 +0300

    Добавлены тесты для square.py, добавлен area_test_report.md

--- square.py
+++ square.py
@@ -1,4 +1,4 @@
-
+import unittest
 
 
 def area(a):
@@ -16,6 +16,13 @@ def perimeter(a):
 	"""
 	return 4 * a
 
+class SquareTestCase(unittest.TestCase):
+	def test_zero_mul(self):
+		res = area(0)
+		self.assertEqual(res, 0)
 
+	def test_square_mul(self):
+		res = area(10)
+		self.assertEqual(res, 100)
 
 
